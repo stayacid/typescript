2b0f5e605e7cd98033c635d0f583d867d2ddad14
Ivan Zagainov <stayacid@gmail.com>
2020-04-19 14:29:52 +0300

    learning classes

---
+++ 4-classes/src/app.ts
@@ -0,0 +1,16 @@
+// Code goes here!
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe() {
+    console.log(`Department: ${this.name}`);
+  }
+}
+
+const accounting = new Department('accounting');
+
+console.log(accounting);
