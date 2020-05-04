e4d8fdaae1bcf72276841ca7d124ea8534f40423
stayacid <stayacid@gmail.com>
2020-05-04 21:02:45 +0300

    finish 13

--- 13-google_maps/src/app.ts
+++ 13-google_maps/src/app.ts
@@ -1,9 +1,13 @@
 // Code goes here!
 import axios from 'axios'
 
+// const
 const form = document.querySelector('form') !
 const addressInput = document.getElementById('address') !as HTMLInputElement
-
+// declare map
+declare let L: any // how to add types?
+const accessToken = 'pk.eyJ1Ijoic3RheWFjaWQiLCJhIjoiY2s5c3E4MzQyMTZuYTN1bjF6ZmZhcjR6eCJ9.pavpKKJa55ymtvKDC-tJvQ'
+// type for response
 type GeocodeResponce = {lat: number, lon: number}[]
 
 function searchAddressHandler(event: Event) {
@@ -13,6 +17,7 @@ function searchAddressHandler(event: Event) {
   axios.get<GeocodeResponce>(`https://cors-anywhere.herokuapp.com/https://nominatim.openstreetmap.org/search?q=${encodeURI(enteredAddress)}&format=json
   `)
   .then(response => {
+    // save coords
     const coordinates: {
       lat: number;
       lon: number;
@@ -22,7 +27,22 @@ function searchAddressHandler(event: Event) {
     }
     coordinates.lat = response.data[0].lat
     coordinates.lon = response.data[0].lon
-    console.log(response);
+    console.log(coordinates);
+
+    // load map
+    document.getElementById('map')!.innerHTML = ''
+    var mymap = L.map('map').setView([coordinates.lat, coordinates.lon], 13);
+    L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${accessToken}`, {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+    // add marker
+    L.marker([coordinates.lat, coordinates.lon]).addTo(mymap)
+
   })
   .catch(err => console.log(err))
 }
