f47250efd1e922c6819e026440af1ee2c32583f2
Ivan Zagainov <stayacid@gmail.com>
2020-05-04 19:04:30 +0300

    set nominatum

--- 13-google_maps/src/app.ts
+++ 13-google_maps/src/app.ts
@@ -2,14 +2,28 @@
 import axios from 'axios'
 
 const form = document.querySelector('form') !
-  const addressInput = document.getElementById('address') !as HTMLInputElement
+const addressInput = document.getElementById('address') !as HTMLInputElement
+
+type GeocodeResponce = {lat: number, lon: number}[]
 
 function searchAddressHandler(event: Event) {
   event.preventDefault()
   const enteredAddress = addressInput.value
 
-  axios.get(`https://nominatim.openstreetmap.org/search${encodeURI(enteredAddress)}`)
-  .then(response => console.log(response))
+  axios.get<GeocodeResponce>(`https://cors-anywhere.herokuapp.com/https://nominatim.openstreetmap.org/search?q=${encodeURI(enteredAddress)}&format=json
+  `)
+  .then(response => {
+    const coordinates: {
+      lat: number;
+      lon: number;
+    } = {
+      lat: 0,
+      lon: 0
+    }
+    coordinates.lat = response.data[0].lat
+    coordinates.lon = response.data[0].lon
+    console.log(response);
+  })
   .catch(err => console.log(err))
 }
 
